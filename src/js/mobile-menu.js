6fa00c4d91d3c51f9724a445237b0ca5162c5a10
Igor Makojda <imakojda@gmail.com>
2021-09-13 23:36:24 +0300

    add java script  function

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -2,22 +2,32 @@
   const mobileMenu = document.querySelector('.js-menu-container');
   const openMenuBtn = document.querySelector('.js-open-menu');
   const closeMenuBtn = document.querySelector('.js-close-menu');
-
+  const closeMenuBtnOne = document.querySelector('.closeMenuBtnOne');
+  const closeMenuBtnTwo = document.querySelector('.closeMenuBtnTwo');
+  const closeMenuBtnThree = document.querySelector('.closeMenuBtnThree');
+  const closeMenuBtnFour = document.querySelector('.closeMenuBtnFour');
+  const closeMenuBtnFive = document.querySelector('.closeMenuBtnFive');
+  const burger = document.querySelector('.burger');
+  const heroBtn = document.querySelector('.heroBtn');
   const toggleMenu = () => {
     const isMenuOpen =
       openMenuBtn.getAttribute('aria-expanded') === 'true' || false;
     openMenuBtn.setAttribute('aria-expanded', !isMenuOpen);
     mobileMenu.classList.toggle('is-open');
-
     const scrollLockMethod = !isMenuOpen
       ? 'disableBodyScroll'
       : 'enableBodyScroll';
     bodyScrollLock[scrollLockMethod](document.body);
+    heroBtn.classList.toggle('hiddenBtn')
   };
-
   openMenuBtn.addEventListener('click', toggleMenu);
   closeMenuBtn.addEventListener('click', toggleMenu);
-
+  closeMenuBtnOne.addEventListener('click', toggleMenu);
+  closeMenuBtnTwo.addEventListener('click', toggleMenu);
+  closeMenuBtnThree.addEventListener('click', toggleMenu);
+  closeMenuBtnFour.addEventListener('click', toggleMenu);
+  closeMenuBtnFive.addEventListener('click', toggleMenu);
+  burger.addEventListener('click', toggleMenu);
   // Закрываем мобильное меню на более широких экранах
   // в случае изменения ориентации устройства.
   window.matchMedia('(min-width: 768px)').addEventListener('change', e => {
